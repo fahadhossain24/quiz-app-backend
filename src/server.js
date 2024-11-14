1c05e778742e5a1442feb57aad14f4d2dbb39c6c
fahadhossain24 <fahadhossaim24@gmail.com>
2024-11-14 09:55:31 +0600

    update

--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
 import mongoose from 'mongoose'
 import config from './config/index.js'
 import app from './app.js'
-const port = 5005
+import http from 'http'
+import {Server} from 'socket.io'
+const port = 5005;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+     methods: ['GET', 'POST'],
+  }
+})
+
+// io.on('connection', (socket) => {
+//   console.log("new user connected")
+//   socket.on('d', (msg) => {
+//     console.log(msg)
+//     socket.emit('d', {msg})
+//   })
+// })
 
 const dbConnection = async () => {
   mongoose.connect(config.database_url)
   console.log('database connection successfull')
-  app.listen(port || config.server_port, () => {
+  server.listen(port || config.server_port, () => {
     console.log('server is listing on port', port || config.server_port)
   })
+
 }
 
 dbConnection()
+
+export default io
