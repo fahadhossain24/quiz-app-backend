43da0fee4ed7ecf47d2b4e15fd5192de450ec0bf
fahadhossain24 <fahadhossaim24@gmail.com>
2024-12-07 10:27:58 +0600

    update

--- src/app/modules/adminModule/admin.routes.js
+++ src/app/modules/adminModule/admin.routes.js
@@ -5,7 +5,7 @@ import s3Upload from "../../middlewares/s3FileUploader.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post('/create', authorization('super-admin'), adminControllers.createAdmin)
+adminRouter.post('/create',  adminControllers.createAdmin)
 adminRouter.get('/retrive/all', adminControllers.getAllAdmin)
 adminRouter.get('/retrive/:id', adminControllers.getSpecificAdmin)
 adminRouter.patch('/update/:id', authorization('super-admin', 'admin'),  adminControllers.updateSpecificAdmin)
